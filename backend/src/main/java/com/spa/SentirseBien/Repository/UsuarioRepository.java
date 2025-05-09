cc6fb4c443de95a1200c825b4e632a985930c6e5
LucianoFK <lucianofk2018@hotmail.com>
2025-05-09 16:19:01 -0300

    Arreglo de errores y pulido del codigo

--- backend/src/main/java/com/spa/SentirseBien/Repository/UsuarioRepository.java
+++ backend/src/main/java/com/spa/SentirseBien/Repository/UsuarioRepository.java
@@ -4,5 +4,7 @@ import com.spa.SentirseBien.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepositroy extends JpaRepository<Cliente, Long> {
-
+ // Métodos personalizados útiles
+    boolean existsByEmail(String email);
+    Optional<Usuario> findByEmail(String email);
 }
